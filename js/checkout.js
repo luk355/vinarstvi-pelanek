3c5a0512051ce336401b7703417b1a3fc2c1a168
Lukas Behal <e-lukas.behal@homecredit.eu>
2020-10-04 22:01:19 +0200

    tidy up cart and checkout

--- js/checkout.js
+++ js/checkout.js
@@ -2,6 +2,7 @@
 simpleCart.ready(function() {
     simpleCart.each(function(item, x) {
       $('form').append('<input type="hidden" name="' + 'Produkt ' + x + '" value="' + item.get('name') + ", " + item.get('quantity') + "ks, cena za lahev:" + item.get('price') + ", dohromady:" + item.get('total') + '">');
+      $('#purchased').append('<tr><td>'+ item.get('name') + '</td><td>' + item.get('price') + 'Kč</td><td>' + item.get('quantity') + 'ks</td><td>' + item.get('total') + 'Kč</td></tr>');
     });
     $('form').append('<input type="hidden" name="' + 'Dohromady" value="' + 'cena celkem: ' + simpleCart.total() + ', vcetne postovneho: ' + simpleCart.grandTotal() + '">');
   });
