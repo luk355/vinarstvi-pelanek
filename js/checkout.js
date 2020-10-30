b8ddd0d8ce71a16c65b52299c40e31cab629fe31
Lukas Behal <e-lukas.behal@homecredit.eu>
2020-10-30 08:07:50 +0100

    chore: send GA statistics

--- js/checkout.js
+++ js/checkout.js
@@ -16,7 +16,7 @@ simpleCart.ready(function () {
   $('form').append('<input type="hidden" name="' + 'Dohromady" value="' + 'cena celkem: ' + simpleCart.total() + ', vcetne postovneho: ' + simpleCart.grandTotal() + '">');
 });
 
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
 
   // get the form elements defined in your form HTML above
   var form = document.getElementById("order-form");
@@ -30,6 +30,13 @@ window.addEventListener("DOMContentLoaded", function() {
     form.reset();
     formSection.style = "display: none ";
     submitSuccessSection.style = "";
+    
+    ga('send', {
+      'hitType': 'pageview',
+      'page': '/purchase-success'
+    });
+    ga('send', 'event', 'wines', 'purchased', 'eshop', simpleCart.total());
+
     simpleCart.empty();
   }
 
@@ -38,7 +45,7 @@ window.addEventListener("DOMContentLoaded", function() {
   }
 
   // handle the form submission event
-  form.addEventListener("submit", function(ev) {
+  form.addEventListener("submit", function (ev) {
     ev.preventDefault();
     var data = new FormData(form);
     ajax(form.method, form.action, data, success, error);
@@ -51,7 +58,7 @@ function ajax(method, url, data, success, error) {
   var xhr = new XMLHttpRequest();
   xhr.open(method, url);
   xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (xhr.readyState !== XMLHttpRequest.DONE) return;
     if (xhr.status === 200) {
       success(xhr.response, xhr.responseType);
